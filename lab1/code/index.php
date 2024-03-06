32dd8b11a34019400940b20ea787f6754d9626fe
linwerr <113230283+linwerr@users.noreply.github.com>
2024-03-06 14:51:33 +0200

    complete task 1

--- lab1/code/index.php
+++ lab1/code/index.php
@@ -1,2 +1,8 @@
 <?php
-echo phpinfo();
+//task 1
+$very_bad_unclear_name = "15 chicken wings";
+
+$order = &$very_bad_unclear_name;
+$order .= " and one fries";
+
+echo "Your order is: $very_bad_unclear_name.<br>";
